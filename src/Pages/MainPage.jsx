98b5783abf67d80e6757e5418feb627b4981a0e8
666wj <666wjli@gmail.com>
2022-03-23 23:07:20 -0700

    update MainPage component with Flex, Button and exchanges list render

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,3 +1,26 @@
-const MainPage = () => <div>temp main page</div>;
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CacheContext } from '../CacheContext';
+import Button from '../StyledComponents/Button';
+import Flex from '../StyledComponents/Flex';
+
+const MainPage = () => {
+  const { cache } = useContext(CacheContext);
+  const navigate = useNavigate();
+
+  return (
+    <Flex>
+      {cache.exchanges.map((exchange) => (
+        <Button
+          type="button"
+          key={exchange.id}
+          onClick={() => navigate(`/coingecko/${exchange.id}`)}
+        >
+          {exchange.name}
+        </Button>
+      ))}
+    </Flex>
+  );
+};
 
 export default MainPage;
