3e815cb85b7c0ab34cc72cc750656aed819b1da8
666wj <666wjli@gmail.com>
2022-03-23 23:47:03 -0700

    update List padding for responsive design

--- src/StyledComponents/List.jsx
+++ src/StyledComponents/List.jsx
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 const List = styled.ul`
   list-style-type: none;
-  padding: 0 5rem;
+  padding: 0;
+  @media (min-width: 480px) {
+    padding: 0 5rem;
+  }
 `;
 
 export default List;
