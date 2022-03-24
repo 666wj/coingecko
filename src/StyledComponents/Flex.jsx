8518ca702ca3935522818b690e3abc022235ceb5
666wj <666wjli@gmail.com>
2022-03-23 23:01:30 -0700

    create styled component Flex

---
+++ src/StyledComponents/Flex.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Flex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding: 3rem;
+`;
+
+export default Flex;
